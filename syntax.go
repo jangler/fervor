a8a33351082b4aaa9874b53e7918b40de165edd4
Brandon Mulcahy <brandon@jangler.info>
2015-06-08 17:34:15 -0400

    Improve golang syntax rules

--- syntax.go
+++ syntax.go
@@ -19,17 +19,19 @@ const (
 var (
 	goRules = []edit.Rule{
 		mustCompile(`//.+?$`, commentId),
-		mustCompile(`/*.*?\*/`, commentId),
+		mustCompile(`/\*.*?\*/`, commentId),
 		mustCompile(`\b(break|case|chan|const|continue|default|defer|else|`+
 			`fallthrough|for|func|go|goto|if|import|interface|map|package|`+
 			`range|return|select|struct|switch|type|var)\b`, keywordId),
-		mustCompile(`\b(close|len|cap|new|make|append|copy|delete|complex|`+
-			`real|imag|panic|recover|print|println)\b`, keywordId),
-		mustCompile(`'.*?'`, literalId),
-		mustCompile(`".*?"`, literalId),
+		mustCompile(`\b(append|cap|close|complex|copy|delete|imag|len|make|`+
+			`new|panic|print|println|real|recover)\b`, keywordId),
+		mustCompile(`\b(bool|byte|complex(64|128)|error|float(32|64)|`+
+			`u?int(8|16|32|64)?|rune|string|uintptr)\b`, keywordId),
+		mustCompile(`'([^']|\\')*?'`, literalId),
+		mustCompile(`"([^"]|\\")*?"`, literalId),
 		mustCompile("`.*?`", literalId),
-		mustCompile(`\b\d+\b`, literalId),
-		mustCompile(`\bnil\b`, literalId),
+		mustCompile(`\b(0[bx])?(\d*\.)?\d+\b`, literalId),
+		mustCompile(`\btrue|false|iota|nil\b`, literalId),
 	}
 	jsonRules = []edit.Rule{
 		mustCompile(`"([^"]|\\")*?":`, keywordId),
