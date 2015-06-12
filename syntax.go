9a42acff4e1f78d294cb46bef90757a8077c5a0a
Brandon Mulcahy <brandon@jangler.info>
2015-06-12 11:56:09 -0400

    Add built-in functions to Python syntax highlighting rules

--- syntax.go
+++ syntax.go
@@ -102,6 +102,14 @@ func pythonRules() []edit.Rule {
 		mustCompile(`\b(and|as|assert|break|class|continue|def|del|elif|else|`+
 			`except|finally|for|from|global|if|import|in|is|lambda|nonlocal|`+
 			`not|or|pass|raise|return|try|while|with|yield)\b`, keywordId),
+		mustCompile(`\b(abs|all|any|ascii|bin|bool|bytearray|bytes|callable|`+
+			`chr|classmethod|compile|complex|delattr|dict|dir|divmod|`+
+			`enumerate|eval|exec|filter|float|format|frozenset|getattr|`+
+			`globals|hasattr|hash|help|hex|id|input|int|isinstance|`+
+			`issubclass|iter|len|list|locals|map|max|memoryview|min|next|`+
+			`object|oct|open|ord|pow|print|property|range|repr|reversed|`+
+			`round|set|setattr|slice|sorted|staticmethod|str|sum|super|`+
+			`tuple|type|vars|zip|__import__)\b`, keywordId),
 		mustCompile(`\b(False|None|True)\b`, literalId),
 		mustCompile(`(\b([rR][bB]|[bB][rR]|\b[uUrR]))?('''(\\?.)*?'''|`+
 			`"""(\\?.)*?"""|"(\.|[^"])*?"|'(\.|[^'])*?')`, literalId),
